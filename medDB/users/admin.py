deaf890c3b3d546a4fb021461f081f60f988139f
HP <hanna.peciak@gmail.com>
2024-04-25 17:06:58 +0200

    Register models

--- medDB/users/admin.py
+++ medDB/users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from .models import Laboratory
+from .models import Employees
+from .models import Project
+from .models import Results
+from .models import Experiments
+from .models import KeyWords
+from .models import Protocols
+from .models import Patients
+
+
+admin.site.register(Laboratory)
+admin.site.register(Employees)
+admin.site.register(Project)
+admin.site.register(Results)
+admin.site.register(Experiments)
+admin.site.register(KeyWords)
+admin.site.register(Protocols)
+admin.site.register(Patients)
 
 
 
-# Register your models here.
+
